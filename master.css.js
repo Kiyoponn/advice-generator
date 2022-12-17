b03f755a0d28223324df4e2a69b0e7f6ae6496e2
Raju Khattri <rajukhattri.86@gmail.com>
2022-12-17 23:57:52 +0530

    Initail commit

---
+++ master.css.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    'light-cyan': '#cee3e9',
+    'neon-green': '#52ffa8',
+    'grayish-blue': {
+      light: '#4e5d73',
+      dark: '$323a49'
+    },
+    'dark-blue': '#1f2632'
+  }
+}
